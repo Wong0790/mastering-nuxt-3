a2cd506b9dadb977adef96c83a95d6e747fe0ef0
Anne <anneris.wong@gmail.com>
2023-08-18 13:42:56 -0400

    Updating auth middleware

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,15 +1,12 @@
 <template>
-  <div
-    v-if="user"
-    class="rounded p-3 flex items-center space-x-3 bg-white"
-  >
+  <div v-if="user" class="rounded p-3 flex items-center space-x-3 bg-white">
     <img
       class="rounded-full w-12 h-12 border-2 border-blue-400"
       :src="profile"
     />
     <div class="text-right">
       <div class="font-medium">{{ name }}</div>
-      <button class="text-sm underline text-slate-500">
+      <button class="text-sm underline text-slate-500" @click="logout">
         Log out
       </button>
     </div>
@@ -18,11 +15,19 @@
 
 <script setup lang="ts">
 const user = useSupabaseUser();
+const { auth } = useSupabaseClient();
 
-const name = computed(
-  () => user?.value.user_metadata.full_name
-);
-const profile = computed(
-  () => user?.value.user_metadata.avatar_url
-);
+const name = computed(() => user.value?.user_metadata.full_name);
+const profile = computed(() => user.value?.user_metadata.avatar_url);
+
+const logout = async () => {
+  const { error } = await auth.signOut();
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  await navigateTo("/login");
+};
 </script>
